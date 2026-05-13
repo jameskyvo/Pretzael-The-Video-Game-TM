97a64206ecef87187ec76e207ba7f07bee384a9e
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2026-05-13 13:18:07 -0500

    Added enemies to the game

---
+++ Assets/Scripts/Utility/MoveTowardsObject.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class MoveTowardsObject : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform otherObject;
+    public float speed = 3.0f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 movementDir = (otherObject.position - transform.position).normalized;
+
+        rb.linearVelocity = movementDir * speed;
+    }
+}
