68721311406f770128209935b58ea23736d8c2c0
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2026-05-12 19:52:15 -0500

    Constrained player to camera space.
    Player can no longer leave the screen

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,10 +7,16 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
     void Start()
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
         rb = GetComponent<Rigidbody2D>();
+
+        // prevents player from leaving the bounds halfway
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
     }
 
     // Update is called once per frame
@@ -23,5 +29,16 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+    }
+
+    void LateUpdate() // Use LateUpdate to ensure all movement logic is finished
+    {
+        Vector3 viewPos = transform.position;
+
+        // Clamp the position within calculated world-space bounds
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+
+        transform.position = viewPos;
     }
 }
