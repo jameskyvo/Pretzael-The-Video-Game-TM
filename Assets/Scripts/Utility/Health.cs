c4368ceb5dcec18a6b9d1047e55cf114a8cce7ac
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2026-06-10 21:56:32 -0500

    Added flicker on damage

--- Assets/Scripts/Utility/Health.cs
+++ Assets/Scripts/Utility/Health.cs
@@ -1,12 +1,18 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int health;
+
+    private SpriteRenderer spriteRenderer;
+    private Color defaultColor;
     void Start()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultColor = spriteRenderer.color;
     }
 
     // Update is called once per frame
@@ -25,6 +31,20 @@ public class Health : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+
+        if (health > 0)
+        {
+            StartCoroutine(FlickerRed());
+        }
+    }
+
+    private IEnumerator FlickerRed()
+    {
+        spriteRenderer.color = Color.red;
+
+        yield return new WaitForSeconds(0.05f);
+
+        spriteRenderer.color = defaultColor;
     }
 
     public void GameOver()
