1f0891f528e32811caaf0be75cc52d5bccf83992
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2026-07-11 21:15:11 -0500

    Added shooter enemy into game

---
+++ Assets/Scripts/Enemy/FireAtPlayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class FireAtPlayer : MonoBehaviour
+{
+    public GameObject shotPrefab;
+    public Transform firePoint;
+    public int secondsBetweenBurstFire;
+    public int attackCooldown;
+
+    private bool isFiring = false;
+    private int shotAmount = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFiring == false)
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+
+    IEnumerator Shoot()
+    {
+        isFiring = true;
+
+        for (int i = 0; i < shotAmount; i++)
+        {
+            Instantiate(shotPrefab, firePoint.position, firePoint.rotation);
+            yield return new WaitForSeconds(secondsBetweenBurstFire);
+        }
+
+        yield return new WaitForSeconds(attackCooldown);
+        isFiring = false;
+    }
+}
