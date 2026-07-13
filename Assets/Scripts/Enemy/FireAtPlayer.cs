c682a94f843ef452e6ef9ecdb493ee50191da615
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2026-07-13 16:52:01 -0500

    Fixed bug where shooters would try to attack the player while out of range

--- Assets/Scripts/Enemy/FireAtPlayer.cs
+++ Assets/Scripts/Enemy/FireAtPlayer.cs
@@ -10,6 +10,8 @@ public class FireAtPlayer : MonoBehaviour
 
     private bool isFiring = false;
     private int shotAmount = 1;
+    private Rigidbody2D rb;
+    private bool outOfBounds = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,7 +21,7 @@ public class FireAtPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isFiring == false)
+        if (isFiring == false && outOfBounds == false)
         {
             StartCoroutine(Shoot());
         }
@@ -42,4 +44,15 @@ public class FireAtPlayer : MonoBehaviour
         yield return new WaitForSeconds(attackCooldown);
         isFiring = false;
     }
+
+    void LateUpdate() 
+    {
+        Vector3 screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        rb = GetComponent<Rigidbody2D>();
+
+        float screenWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
+        float objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
+
+        outOfBounds = transform.position.x > screenBounds.x || transform.position.y > screenBounds.y || transform.position.x < -screenBounds.x || transform.position.y < -screenBounds.y;
+}
 }
